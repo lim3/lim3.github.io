fdb6070dff809a9907b51b0c91e213534eaa0bba
Emil Hoefling <emil@viper.dev>
2022-04-18 11:21:37 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<section class="hero">
+    <div class="wrapper">
+        <h1>lim3</h1>
+        <h2>UX / UI | Webdesign und -Entwicklung</h2>
+        <a href="mailto:email@lim3.de" class="button">schreib mir</a>
+    </div>
+</section>
+
+<style lang="scss">
+    .hero {
+        display: flex;
+        min-height: 100vh;
+        text-align: center;
+    }
+
+    .wrapper {
+        margin: auto;
+    }
+
+    h1 {
+        font-size: 6rem;
+        font-weight: 100;
+    }
+    h2 {
+        font-weight: 200;
+        margin-bottom: 2rem;
+    }
+</style>
